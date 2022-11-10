28d79b8ef84f768d9c4346cf030398747ec2b57a
hiiro <bachlong12623@gmail.com>
2022-11-10 09:57:39 +0700

    Add project files.

---
+++ MyApp/MyMath.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    public class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            int tong;
+            tong = a + b;
+            return tong;
+        }
+
+        public int Sub(int a, int b)
+        {
+            int hieu;
+            hieu = a - b;
+            return hieu;
+        }
+
+        public int Mul(int a, int b)
+        {
+            int tich;
+            tich = a * b;
+            return tich;
+        }
+
+        public int Div(int a, int b)
+        {
+            int thuong;
+            if (b == 0)
+            {
+                return 9999;
+            }
+            else
+            {
+                thuong = a / b;
+                return thuong;
+            }
+        }
+
+        public bool isNumber(string pValue)
+        {
+            foreach (Char c in pValue)
+            {
+                if (!Char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ MyAppTest/MyAddTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MyApp;
+
+namespace MyAppTest
+{
+    [TestFixture]
+    public class MyAddTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            MyMath m = new MyMath();
+            Assert.AreEqual(15, m.Add(10, 5),"False");
+        }
+        [Test]
+        public void MyTest1()
+        {
+            MyMath m = new MyMath();
+            Assert.AreEqual(10, m.Add(5, 5),"True");
+        }
+    }
+}
